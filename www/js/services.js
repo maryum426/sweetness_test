b98b935ecbe233d9dd85805db65c3d14f02ef6d9
unknown <maryum.babar@KL-14.kualitatem.local>
2013-10-01 11:58:21 +0500

    adding parse.com to access origin

--- www/js/services.js
+++ www/js/services.js
@@ -290,11 +290,11 @@ angular.module('DataServices', ['ngResource'])
         var checkPhoneInDB = function (phone , cb) {
             var query = new Parse.Query("Auth");
             query.equalTo("phone", phone);
-            console.log(window.location);
+           
             query.find({
                 success:function (r_auth) {
                     if (r_auth.length > 0) {
-                        cb(r_auth[0])
+                        cb(r_auth[0]);
                     }
                     else {
                        cb(null);
