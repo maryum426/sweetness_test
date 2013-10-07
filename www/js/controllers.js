572da9dc7ada9c1d4a71290c7651305d80f34812
unknown <Administrator@KL-14.kualitatem.local>
2013-10-07 15:28:02 +0500

    check fb.login 13

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3608,19 +3608,7 @@ function AuthController($log, $scope, authService, $location, CONSTANTS, faceboo
             $scope.section.loginInProgressMsg = CONSTANTS.LOGIN_IN_PROGRESS;
         });*/
         
-        alert('Device is ready! Make sure you set your app_id below this alert.');
-        try{                              
-        window.fbAsyncInit = function () {
-                                      FB.init({ appId: '366407670138696', nativeInterface: CDV.FB, useCachedDialogs: false });
-                                      
-                                      //FB.getLoginStatus(function(response){
-                                      fbApiInit = true;
-
-        };
-                                      } catch (e) {
-                                      alert("Hello: "+e);
-                                      }
-                                      
+       alert("facebookLogin func()");
         if (window.fbApiInit)
         {
             alert ("Initialization Okay");
