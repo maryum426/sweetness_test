7dfcfd8fbd8a8a164a2b7026b6190599f5496f6b
unknown <Administrator@KL-14.kualitatem.local>
2013-10-07 12:55:50 +0500

    check fb.login 8

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3607,6 +3607,21 @@ function AuthController($log, $scope, authService, $location, CONSTANTS, faceboo
             $scope.section.loginInProgress = true;
             $scope.section.loginInProgressMsg = CONSTANTS.LOGIN_IN_PROGRESS;
         });*/
+        
+        document.addEventListener('deviceready', function() {
+                                      try {
+                                      alert('Device is ready! Make sure you set your app_id below this alert.');
+                                      window.fbAsyncInit = function () {
+                                      FB.init({ appId: '366407670138696', nativeInterface: PG.FB, useCachedDialogs: false });
+                                      
+                                      //FB.getLoginStatus(function(response){
+                                      fbApiInit = true;
+
+                                      }
+                                      } catch (e) {
+                                      alert("Hello: "+e);
+                                      }
+                                      }, false);
         if (window.fbApiInit)
         {
             alert ("Initialization Okay");
